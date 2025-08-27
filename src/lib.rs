49e199b14641aa8522522bc56fec505251e2b1f4
LouKeen <patricklouie.pl@gmail.com>
2025-08-27 21:03:31 +0800

    Add unit tests for `validate_answer` function in lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,26 @@
 pub fn validate_answer(answer: &str) -> bool {
     if answer.len() != 5 {
         println!("word must be exactly 5 letters");
-        return true
+        return true;
     }
     false
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::validate_answer;
+
+    #[test]
+    fn should_result_in_false_when_answer_is_exactly_5_letters() {
+        assert_eq!(validate_answer("abcde"), false);
+    }
+
+    #[test]
+    fn should_result_in_true_when_answer_is_not_5_letters() {
+        assert_eq!(validate_answer("a"), true);
+        assert_eq!(validate_answer("ab"), true);
+        assert_eq!(validate_answer("abc"), true);
+        assert_eq!(validate_answer("abcd"), true);
+        assert_eq!(validate_answer("abcdef"), true);
+    }
 }
